fb5a9c1d3303105e2f14fee021f8a3e714de9141
Samuel Abu <abu.sammie@gmail.com>
2021-04-06 15:20:40 +0200

    onclick event added

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,10 +11,14 @@ const Header = ({ title }) => {
 }*/
 
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click')
+    }
+
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' />
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
