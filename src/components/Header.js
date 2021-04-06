d2aebd12ea78035ca9cac31cc358a83f2aad8efa
Samuel Abu <abu.sammie@gmail.com>
2021-04-06 11:33:07 +0200

    some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,19 @@
 import PropTypes from 'prop-types';
 
-
+/* CSS in JS
 const Header = ({ title }) => {
     return (
         <header>
+            <h1 style={headingStyle}>{title}</h1>
+        </header>
+    )
+}*/
+
+const Header = ({ title }) => {
+    return (
+        <header className='header'>
             <h1>{title}</h1>
+            <button className='btn'>Add</button>
         </header>
     )
 }
@@ -16,5 +25,11 @@ Header.defaultProps = {
 Header.propTyes = {
     title: PropTypes.string.isRequired,
 }
+
+/*Styling for CSS in JS
+const headingStyle = {
+    color: 'red', 
+    backgroundColor: 'black'
+}*/
 
 export default Header;
