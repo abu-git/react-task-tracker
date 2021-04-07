0ba1aad68e0986cd9d3f84e8311c878f68ebfe8b
Samuel Abu <abu.sammie@gmail.com>
2021-04-07 13:15:16 +0200

    submit functionality added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,12 +1,26 @@
 import { useState } from 'react'
 
-const AddTask = () => {
+const AddTask = ({ onAdd }) => {
     const [text, setText ] = useState('')
     const [day, setDay] = useState('')
     const [reminder, setReminder] = useState(false)
 
+    const onSubmit = (e) => {
+        e.preventDefault()
+        if(!text){
+            alert('Please add Task')
+            return
+        }
+
+        onAdd({ text, day, reminder})
+
+        setText('')
+        setDay('')
+        setReminder(false)
+    }
+
     return (
-        <form className='add-form'>
+        <form className='add-form' onSubmit={onSubmit}>
             <div className='form-control'>
                 <label>Task</label>
                 <input type='text' 
@@ -26,6 +40,7 @@ const AddTask = () => {
             <div className='form-control form-control-check'>
                 <label>Set Reminder</label>
                 <input type='checkbox'
+                    checked={reminder}
                     value={reminder}
                     onChange={(e) => setReminder(e.currentTarget.checked)} 
                 />
