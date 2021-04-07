0d6b4211c5e72c34b4d64c2bc6a7953121a027d9
Samuel Abu <abu.sammie@gmail.com>
2021-04-07 15:52:50 +0200

    add task to server works

--- src/App.js
+++ src/App.js
@@ -25,16 +25,28 @@ function App() {
   }
 
   //Add Task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 100000) + 1
-    const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch('http://localhost:5000/tasks', 
+      { method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(task)
+    })
+    
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+
+
+    //const id = Math.floor(Math.random() * 100000) + 1
+    //const newTask = { id, ...task }
+    //setTasks([...tasks, newTask])
   }
 
   //Delete Task
   const deleteTask = async (id) => {
-    await fetch(`http://localhost:5000/tasks/${id}`, 
-    { method: 'DELETE'})
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE'})
 
     setTasks(tasks.filter((task) => task.id !== id))
   }
